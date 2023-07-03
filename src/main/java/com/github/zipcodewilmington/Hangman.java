771c252c9701e6ce2705bee1b87d5697ebc79560
maggie <snydermag@gmail.com>
2023-07-03 18:09:59 -0400

    added my name

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -5,6 +5,11 @@ package com.github.zipcodewilmington;
  * @author xt0fer
  * @version 1.0.0
  * @date 5/27/21 11:02 AM
+ *
+ * Additional code by: Maggie Snyder
+ *
  */
 public class Hangman {
+
+
 }
